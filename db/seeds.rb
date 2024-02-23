b2469b8d30cfe9d0b1262e8343f773807625905c
roundshape <99551120+roundshape@users.noreply.github.com>
2024-02-23 17:50:26 +0900

    プロトタイプ投稿機能firstCommit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# ユーザーデータを作成
+user = User.create!(
+  email: 'motoi.kasuya@roundshape.jp',
+  password: 'password',
+  password_confirmation: 'password',
+  name: 'motoi',
+  profile: 'これはテストプロフィールです。',
+  occupation: 'デベロッパー',
+  position: 'マネージャー'
+)
+
+# さらに複数のユーザーデータを挿入する場合は、同様にcreate!メソッドを使用します。
+# 例:
+# User.create!(email: '...', password: '...', ...)
+
+# prototype = Prototype.create!(
+#   title: "テストプロトタイプ",
+#   catch_copy: "これはキャッチコピーです。",
+#   concept: "これはコンセプトです。",
+#   user_id: user.id # userカラムにユーザーIDを指定
+# )
+#prototype.image.attach(io: File.open('/users/motoi/Desktop/IMG_3966low.JPG'), filename: 'IMG_3966low.JPG')
+
+
+prototype = Prototype.new(
+ title: "テストプロトタイプ",
+ catch_copy: "これはキャッチコピーです。",
+  concept: "これはコンセプトです。",
+  user_id: user.id
+)
+
+prototype.image.attach(io: File.open('/users/motoi/Desktop/IMG_3966low.JPG'), filename: 'IMG_3966low.JPG')
+
+if prototype.image.attached?
+  puts "Image is attached."
+else
+  puts "Image is not attached."
+end
+
+if prototype.save
+  puts "Prototype saved successfully."
+else
+  puts prototype.errors.full_messages
+end
